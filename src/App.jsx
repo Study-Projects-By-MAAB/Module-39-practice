0903aff02e2065e3ff39ee102fa6d9dbd6c84d99
maab-fw <maabfw@proton.me>
2024-03-10 00:01:25 +0600

    documentation write...

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,61 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Vite + React</h1> <hr /> <br />
+      <Profile /> <hr />
+      <MyApp /> <hr /> <br />
+      <AboutPage /> <hr /> <br />
     </>
   )
 }
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  )
+}
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Welcome To my apps</h1>
+      <MyButton />
+    </div>
+  )
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there. <br /> How do you do</p>
+
+    </>
+  )
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
 
 export default App
